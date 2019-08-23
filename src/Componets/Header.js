f71101ee5adcc0c783e674c39c71595bd69e0b5d
GustavoAmiel <amielurbina@gmail.com>
2019-08-22 20:32:52 -0500

    Se agrego un logotipo con dimenciones dentro del menu.

--- src/Componets/Header.js
+++ src/Componets/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Nav, Navbar, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+import logo from '../images/dbz.jpg';
+import { Nav, Navbar, NavDropdown, Form, FormControl, Button, Image } from 'react-bootstrap'
 
 //import logo from '../images/logo.svg';
 
@@ -7,24 +8,26 @@ class Header extends React.Component {
     render() {
         return (
             <div>
-                <Navbar bg="light" expand="lg">
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <Navbar.Brand href="#home">
+                        <Image src={logo} roundedCircle width="45" height="45" />
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#home">Inicio</Nav.Link>
                             <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown title="Opciones" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Item</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Item</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Item</NavDropdown.Item>
                                 <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.4">Separando link</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
                         <Form inline>
-                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                            <Button variant="outline-success">Search</Button>
+                            <FormControl type="text" placeholder="Buscar" className="mr-sm-2" />
+                            <Button variant="outline-success">Buscar</Button>
                         </Form>
                     </Navbar.Collapse>
                 </Navbar>
