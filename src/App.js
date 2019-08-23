2f8551890f5ea2b63a06f295ae7e37223c96208d
GustavoAmiel <amielurbina@gmail.com>
2019-08-22 20:05:12 -0500

    Se importa el componente Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Componets/Header';
 
 function App() {
   return (
+    
     <div className="App">
+      <div><Header/></div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
